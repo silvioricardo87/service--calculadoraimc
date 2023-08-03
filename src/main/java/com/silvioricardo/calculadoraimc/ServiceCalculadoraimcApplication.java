cbf3077a429c2fa12c80d338e4258e3742e9fe4f
Silvio Chagas <git@silvioricardo.com>
2023-08-03 11:26:09 -0300

    feat: projeto base para implementação

---
+++ src/main/java/com/silvioricardo/calculadoraimc/ServiceCalculadoraimcApplication.java
@@ -0,0 +1,13 @@
+package com.silvioricardo.calculadoraimc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCalculadoraimcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceCalculadoraimcApplication.class, args);
+	}
+
+}
