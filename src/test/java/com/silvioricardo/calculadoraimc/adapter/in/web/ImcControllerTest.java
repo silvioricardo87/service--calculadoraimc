2c24f90fb10f95c741262b1f5097813b6bac9042
Silvio Chagas <git@silvioricardo.com>
2023-08-05 13:20:15 -0300

    tests: adicionado mock do imc e assert para os valores

--- src/test/java/com/silvioricardo/calculadoraimc/adapter/in/web/ImcControllerTest.java
+++ src/test/java/com/silvioricardo/calculadoraimc/adapter/in/web/ImcControllerTest.java
@@ -1,8 +1,13 @@
 package com.silvioricardo.calculadoraimc.adapter.in.web;
 
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.silvioricardo.calculadoraimc.domain.Imc;
+import com.silvioricardo.calculadoraimc.domain.ImcClassificacao;
 import com.silvioricardo.calculadoraimc.usecase.ImcUseCase;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,11 +26,18 @@ class ImcControllerTest {
 
   @Test
   void deveCalcularImc() throws Exception {
+    Imc mockImc = new Imc(1.80, 80.0, 24.69, ImcClassificacao.NORMAL);
+
+    when(imcUseCase.calcularImc(80.0, 1.80)).thenReturn(mockImc);
 
     mockMvc.perform(get("/imc")
       .param("peso", "80")
       .param("altura", "1.80"))
-      .andExpect(status().isOk());
+      .andExpect(status().isOk())
+      .andExpect(jsonPath("$.altura", is(1.80)))
+      .andExpect(jsonPath("$.peso", is(80.0)))
+      .andExpect(jsonPath("$.valorImc", is(24.69)))
+      .andExpect(jsonPath("$.grauObesidade", is("NORMAL")));
   }
 
   @Test
